77a2d259c148bb59462bc58cc27b955e2eadde1e
Alan Vinicius <alan123vinicius@gmail.com>
2020-11-05 21:30:14 -0300

    Update index2.js

--- index2.js
+++ index2.js
@@ -1,1 +1,25 @@
 
+<script>
+
+    var aux = [
+     "a","b","c","d"
+    ];
+
+    var aux2 = [];
+    
+    localStorage.setItem("aux", aux);
+    
+    console.log(aux);
+
+    var aux2 = aux.splice(2,1);
+
+    localStorage.setItem("aux2", aux2);
+
+    localStorage.setItem("aux", aux2);
+
+    localStorage.getItem("aux2");
+
+    console.log(aux);
+    console.log(aux2); 
+    
+</script>
